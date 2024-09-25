d41a8556d7046a9746bd5f2f7eef57228b9ab732
Bryant MacMahon <bryantmac@gmail.com>
2024-09-25 08:25:49 -0700

    updating new params

--- api/index.js
+++ api/index.js
@@ -1,167 +1,199 @@
-require('dotenv').config();
-
+// require('dotenv').config();
+import dotenv from "dotenv";
+import { extractListingPrice } from "./extractListingPrice.js";
+dotenv.config();
 
 const TELEGRAM_BOT_TOKEN = process.env.BOT_TOKEN;
-
 // Main
 if (process.env.PROD) {
-    var TELEGRAM_CHAT_ID = process.env.MAIN_CHAT;
+  var TELEGRAM_CHAT_ID = process.env.MAIN_CHAT;
 }
 // Test
 else {
-    var TELEGRAM_CHAT_ID = process.env.TEST_CHAT;
+  var TELEGRAM_CHAT_ID = process.env.TEST_CHAT;
 }
-
 const HELIUS_API_KEY = process.env.HELIUS_KEY;
 const HELIUS_RPC_URL = `https://mainnet.helius-rpc.com/?api-key=${HELIUS_API_KEY}`;
-
-
+const ROYALTY_FEE = 0.08;
 
 // Vercel API handler
 export default async function (req, res) {
-    if (req.method === 'POST') {
-        const requestBody = req.body;
+  if (req.method === "POST") {
+    const requestBody = req.body;
 
-        if (requestBody[0].type !== 'TRANSFER') {
-            console.log(requestBody[0].signature);
+    if (requestBody[0].type !== "TRANSFER") {
+      console.log(requestBody[0].signature);
 
-            const transactionData = await checkTransactionStatus(requestBody[0].signature);
-            const action = extractTransactionType(transactionData.meta.logMessages);
-            console.log(action);
-            console.log("Transaction confirmed:", JSON.stringify(transactionData, null, 2));
+      const transactionData = await checkTransactionStatus(
+        requestBody[0].signature
+      );
+      const action = extractTransactionType(transactionData.meta.logMessages);
+      console.log(action);
+      console.log(
+        "Transaction confirmed:",
+        JSON.stringify(transactionData, null, 2)
+      );
 
-            const Transfertimestamp = new Date(requestBody[0].timestamp * 1000).toLocaleString();
-            const Transfersignature = `https://solana.fm/tx/${requestBody[0].signature}`;
+      const Transfertimestamp = new Date(
+        requestBody[0].timestamp * 1000
+      ).toLocaleString();
+      const Transfersignature = `https://solana.fm/tx/${requestBody[0].signature}`;
 
-            let url, mp, index;
-            if (requestBody[0].instructions[2].programId === 'M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K') {
-                // Handle Magic Eden
-                url = `https://magiceden.us/marketplace/hegends?activeTab=myItems&solItemDetailsModal=`;
-                mp = 'Magic Eden';
-                index = 4;
-            } else if (requestBody[0].instructions[2].programId === 'TCMPhJdwDryooaGtiocG1u3xcYbRpiJzb283XfCZsDp') {
-                // Handle Tensor
-                url = `https://www.tensor.trade/item/`;
-                mp = 'Tensor';
-                index = (action === 'Delist' || action === 'Listing') ? 0 : 2;
-            } else {
-                url = "";
-                mp = '';
-                index = 4;
-            }
+      let url, mp, index;
+      if (
+        requestBody[0].instructions[2].programId ===
+        "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
+      ) {
+        // Handle Magic Eden
+        url = `https://magiceden.us/marketplace/hegends?activeTab=myItems&solItemDetailsModal=`;
+        mp = "Magic Eden";
+        index = 4;
+      } else if (
+        requestBody[0].instructions[2].programId ===
+        "TCMPhJdwDryooaGtiocG1u3xcYbRpiJzb283XfCZsDp"
+      ) {
+        // Handle Tensor
+        url = `https://www.tensor.trade/item/`;
+        mp = "Tensor";
+        index = action === "Delist" || action === "Listing" ? 0 : 2;
+      } else {
+        url = "";
+        mp = "";
+        index = 4;
+      }
 
-            const NFTmintAddress = requestBody[0].instructions[2]['accounts'][index];
-            console.log(NFTmintAddress);
-            const mintUrl = `https://solana.fm/address/${NFTmintAddress}`;
-            const asset = await getAssetImageUrl(NFTmintAddress);
-            const im = asset.content.links.image;
-            const name = asset.content.metadata.name;
-            const desc = asset.content.metadata.description;
-            url += NFTmintAddress;
+      const NFTmintAddress = requestBody[0].instructions[2]["accounts"][index];
+      console.log(NFTmintAddress);
+      const mintUrl = `https://solana.fm/address/${NFTmintAddress}`;
+      const listingPriceInLamports = await extractListingPrice(requestBody);
+      const listingPriceInSol = (listingPriceInLamports / 1000000000).toFixed(
+        2
+      );
 
-            const messageToSendTransfer =
-                `<b>New ${action}!</b>\n\n<b>${name}</b>\n${desc}\n\n<b>Market:</b> <a href='${url}'>${mp}</a>\n\n<a href='${Transfersignature}'>TX</a> | <a href='${mintUrl}'>Mint</a> `;
+      // Add an 8% royalty fee
+      const priceWithRoyalties = (
+        listingPriceInSol *
+        (1 + ROYALTY_FEE)
+      ).toFixed(2);
+      
+      console.log({ priceWithRoyalties });
+      const asset = await getAssetImageUrl(NFTmintAddress);
+      const im = asset.content.links.image;
+      const name = asset.content.metadata.name;
+      const desc = asset.content.metadata.description;
+      url += NFTmintAddress;
 
-            if (action === 'Sell' || action === 'Listing') {
-                await sendToTelegramNFT(messageToSendTransfer, im);
-            }
-        } else {
-            console.log('Transfer Transaction:');
-        }
+      const messageToSendTransfer = `<b>New ${action}!</b>\n\n<b>${name}</b>\n${desc}\n\n<b>Market:</b> <a href='${url}'>${mp}</a>\n\n<a href='${Transfersignature}'>TX</a> | <a href='${mintUrl}'>Mint</a> `;
 
-        res.status(200).send('Logged POST request body.');
+      if (action === "Sell" || action === "Listing") {
+        await sendToTelegramNFT(messageToSendTransfer, im);
+      }
     } else {
-        res.status(405).send('Method not allowed.');
+      console.log("Transfer Transaction:");
     }
+
+    res.status(200).send("Logged POST request body.");
+  } else {
+    res.status(405).send("Method not allowed.");
+  }
 }
 
 // This function is used to check the transaction status
 async function checkTransactionStatus(signature) {
-    let transactionData = null;
-    let attempts = 0;
-    const maxAttempts = 10;
-    const delay = 5000;
+  let transactionData = null;
+  let attempts = 0;
+  const maxAttempts = 10;
+  const delay = 5000;
 
-    while (transactionData === null && attempts < maxAttempts) {
-        attempts++;
-        console.log(`Attempt ${attempts} to check transaction status...`);
+  while (transactionData === null && attempts < maxAttempts) {
+    attempts++;
+    console.log(`Attempt ${attempts} to check transaction status...`);
 
-        const response = await fetch(HELIUS_RPC_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                jsonrpc: '2.0',
-                id: '1',
-                method: 'getTransaction',
-                params: [signature, { commitment: 'confirmed', encoding: 'json' }],
-            }),
-        });
+    const response = await fetch(HELIUS_RPC_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        id: "1",
+        method: "getTransaction",
+        params: [signature, { commitment: "confirmed", encoding: "json" }],
+      }),
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        if (data.result !== null) {
-            transactionData = data.result;
-            console.log('Transaction confirmed');
-            return transactionData;
-        } else {
-            console.log('Transaction not confirmed yet. Retrying...');
-            await new Promise(resolve => setTimeout(resolve, delay));
-        }
+    if (data.result !== null) {
+      transactionData = data.result;
+      console.log("Transaction confirmed");
+      return transactionData;
+    } else {
+      console.log("Transaction not confirmed yet. Retrying...");
+      await new Promise((resolve) => setTimeout(resolve, delay));
     }
+  }
 
-    console.log('Max attempts reached, transaction still not confirmed.');
-    return transactionData;
+  console.log("Max attempts reached, transaction still not confirmed.");
+  return transactionData;
 }
 
 // This function checks the marketplace action
 function extractTransactionType(logMessages) {
-    let transactionType = 'Unknown';
-    logMessages.forEach(log => {
-        if (log.includes('Instruction: CoreBuy') || log.includes('Instruction: BuyCore')) {
-            transactionType = 'Sell';
-        } else if (log.includes('Instruction: CoreCancelSell') || log.includes('Instruction: DelistCore')) {
-            transactionType = 'Delist';
-        } else if (log.includes('Instruction: CoreSell') || log.includes('Instruction: ListCore')) {
-            transactionType = 'Listing';
-        }
-    });
-    return transactionType;
+  let transactionType = "Unknown";
+  logMessages.forEach((log) => {
+    if (
+      log.includes("Instruction: CoreBuy") ||
+      log.includes("Instruction: BuyCore")
+    ) {
+      transactionType = "Sell";
+    } else if (
+      log.includes("Instruction: CoreCancelSell") ||
+      log.includes("Instruction: DelistCore")
+    ) {
+      transactionType = "Delist";
+    } else if (
+      log.includes("Instruction: CoreSell") ||
+      log.includes("Instruction: ListCore")
+    ) {
+      transactionType = "Listing";
+    }
+  });
+  return transactionType;
 }
 
 // This function sends the NFT updates to Telegram
 async function sendToTelegramNFT(message, imageUrl) {
-    const telegramUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendPhoto`;
-    const response = await fetch(telegramUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            chat_id: TELEGRAM_CHAT_ID,
-            photo: imageUrl,
-            caption: message,
-            parse_mode: "HTML"
-        }),
-    });
-    const responseData = await response.json();
+  const telegramUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendPhoto`;
+  const response = await fetch(telegramUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: TELEGRAM_CHAT_ID,
+      photo: imageUrl,
+      caption: message,
+      parse_mode: "HTML",
+    }),
+  });
+  const responseData = await response.json();
 
-    if (!response.ok) {
-        console.error('Failed to send photo to Telegram:', responseData);
-    }
+  if (!response.ok) {
+    console.error("Failed to send photo to Telegram:", responseData);
+  }
 }
 
 // This function gets images associated with NFTs
 async function getAssetImageUrl(mintAddress) {
-    const response = await fetch(HELIUS_RPC_URL, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            jsonrpc: '2.0',
-            id: '1',
-            method: 'getAsset',
-            params: { id: mintAddress },
-        }),
-    });
-    const result = await response.json();
-    return result.result;
+  const response = await fetch(HELIUS_RPC_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      id: "1",
+      method: "getAsset",
+      params: { id: mintAddress },
+    }),
+  });
+  const result = await response.json();
+  return result.result;
 }
